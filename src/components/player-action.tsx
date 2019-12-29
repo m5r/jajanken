e8686c2a4edd3b2ac0fbc4911a6d7ffcfb966325
m5r <mokht@rmi.al>
2019-12-29 15:21:04 +0100

    use Modernizr to serve webp when possible and animate differently the micro-interaction with the player actions

--- src/components/player-action.tsx
+++ src/components/player-action.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useState } from "react";
 
 import { Move } from "../shared-types";
 
@@ -13,13 +13,21 @@ const base64Images: Record<Move, string> = {
 	SCISSORS: require("../../public/static/images/scissors.png.b64"),
 };
 
-const PlayerAction: FunctionComponent<Props> = ({ onClick, move }) => (
-	<img
-		className="flex max-h-full w-auto justify-around cursor-pointer jjk-player-action"
-		src={`data:image/png;base64,${base64Images[move]}`}
-		onClick={onClick}
-		alt={move}
-	/>
-);
+const PlayerAction: FunctionComponent<Props> = ({ onClick, move }) => {
+	const [shouldTranslateToTop, setShouldTranslateToTop] = useState<Boolean>(false);
+
+	return (
+		<img
+			className={`flex max-h-full w-auto justify-around cursor-pointer jjk-player-action ${shouldTranslateToTop ? "jjk-bump" : ""}`}
+			src={`data:image/png;base64,${base64Images[move]}`}
+			onClick={() => {
+				onClick();
+				setShouldTranslateToTop(true);
+			}}
+			onTransitionEnd={() => setShouldTranslateToTop(false)}
+			alt={move}
+		/>
+	);
+};
 
 export default PlayerAction;
