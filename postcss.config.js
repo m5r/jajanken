eca08c420c5ece8867fa42b8145c3fbc34106a21
m5r <mokht@rmi.al>
2020-03-12 00:14:55 +0100

    update postcss config

--- postcss.config.js
+++ postcss.config.js
@@ -1,18 +1,19 @@
-module.exports = {
-	plugins: [
-		"tailwindcss",
-		"postcss-preset-env",
-		process.env.NODE_ENV === "production" ?
-			[
-				"@fullhuman/postcss-purgecss",
-				{
-					content: [
-						"./src/pages/**/*.{js,jsx,ts,tsx}",
-						"./src/components/**/*.{js,jsx,ts,tsx}",
-					],
-					defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
-				},
-			] :
-			undefined,
-	],
-};
+let plugins = [
+	"tailwindcss",
+	"postcss-preset-env",
+];
+
+if (process.env.NODE_ENV === "production") {
+	plugins.push([
+		"@fullhuman/postcss-purgecss",
+		{
+			content: [
+				"./src/pages/**/*.{js,jsx,ts,tsx}",
+				"./src/components/**/*.{js,jsx,ts,tsx}",
+			],
+			defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || [],
+		},
+	]);
+}
+
+module.exports = { plugins };
