b70dcebaf7625e2a294fab967a22586412f21376
m5r <mokht@rmi.al>
2022-07-02 02:22:58 +0200

    oops no type-only imports

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import type { AppProps } from "next/app";
+import { AppProps } from "next/app";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import * as Fathom from "fathom-client";
